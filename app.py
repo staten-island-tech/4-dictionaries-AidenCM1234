93f884be78f81982824b6a60919dff5e151f0251
AidenCM1234 <aidenm124@nycstudents.net>
2025-10-06 09:05:06 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+dict_apple = {"name":"Apple","price":5.0,"brand":"ShopRite"}
+dict_pear = {"name":"Pear","price":4.0,"brand":"KirkLand"}
+dict_kangaroo = {"name":"Kangaroo","price":559.99,"brand":"Austrila"}
+
+store=[dict_apple, dict_pear, dict_kangaroo]
+for item in store:
+    print (f"{item["name"]}" f" ${item["price"]}" f" ({item["brand"]})")
+#print(store)
+
+isitem = False
+while isitem == False:
+    choice = input("Please choose one item to purchase: ")
+    for item in store:
+        if choice == item["name"]:
+            print(f"You have purchased one {item["name"]} for ${item["price"]}")
+            isitem = True
+    if isitem == False:
+        print ("Try again")
