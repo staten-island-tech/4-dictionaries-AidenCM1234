4a5bbe4e062f86095145d9c369ce0ed880d3aa34
AidenCM1234 <aidenm124@nycstudents.net>
2025-10-09 09:24:25 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ dict_kangaroo = {"name":"Kangaroo","price":559.99,"brand":"Austrila"}
 
 store=[dict_apple, dict_pear, dict_kangaroo]
 for item in store:
-    print (f"{item["name"]}" f" ${item["price"]}" f" ({item["brand"]})")
+    print (f"{item["name"]} ${item["price"]} ({item["brand"]})")
 #print(store)
 
 isitem = False
