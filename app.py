884ecbc3a32601b22808e4d31526d899386e6034
AidenCM1234 <aidenm124@nycstudents.net>
2025-10-09 09:16:17 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,12 +8,16 @@ for item in store:
 #print(store)
 
 isitem = False
-while isitem == False:
-    choice = input("Please choose one item to purchase: ")
-    for item in store:
-        if choice == item["name"]:
-            print(f"You have purchased one {item["name"]} for ${item["price"]}")
-            isitem = True
-            again = input("")
-    if isitem == False:
-        print ("Try again")
+done = False
+while done == False:
+    while isitem == False:
+        choice = input("Please choose one item to purchase: ")
+        for item in store:
+            if choice == item["name"]:
+                print(f"You have purchased one {item["name"]} for ${item["price"]}")
+                isitem = True
+                again = input("Would you like to purchase more Y or N: ")
+                if again == "N" or again == "n":
+                    done = True
+        if isitem == False:
+            print ("Try again")
