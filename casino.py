091bf4032851abbd7ea234760ba708632b0a1e8e
AidenCM1234 <aidenm124@nycstudents.net>
2025-10-24 08:43:29 -0400

    Update casino.py

--- casino.py
+++ casino.py
@@ -1,25 +1,28 @@
 def casino (x,a,b,c):
     y=0
-    while not x<=0:
-        x-=1
-        a+=1
+    x+=1
+    while not x<=1:
         y+=1
+        a+=1
         if a == 35:
             x+=30
             a=0
-        
         x-=1
-        b+=1
+        print(x)
         y+=1
+        b+=1
         if b == 100:
             x+=60
             b=0
-        
         x-=1
-        c+=1
+        print(x)
         y+=1
+        c+=1
         if c == 10:
-            x+=9
+            x=x+9
             c=0
-    print(y)
-casino(48,3,10,6)
+        x-=1
+        print(x)
+    print(f"Martha plays {y} times before going broke")
+casino(77,4,9,3)
+#casino(48,3,10,4)
