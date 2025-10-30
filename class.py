ccf41aaabeb526d728f65ac224a53d4a19ab1b3e
AidenCM1234 <aidenm124@nycstudents.net>
2025-10-30 09:25:49 -0400

    Create class.py

---
+++ class.py
@@ -0,0 +1,12 @@
+
+class hero:
+    def __init__(self, name, money, inventory):
+        self.name = name
+        self.money = money
+        self.inventory = inventory
+    def buy(self, item):
+        self.inventory.append(item)
+        print (f"{self.name} purchased {item} and has {self.inventory}")
+Nathan =hero("Nathan", 0, ["Pencil"])
+print(Nathan.__dict__)
+Nathan.buy("Xi Yang")
